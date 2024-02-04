f48efef3b816b10491424587654c09fed398cd3a
saeed padkan <s.padkan@gmail.com>
2024-02-04 22:59:16 +0100

    setup project typeScript React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactElement } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
+import { Dashboard } from './pages/dashboard';
+const App: FC = (): ReactElement => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
+  );
+};
+
+export default App;
